a048051e4b4ec25072835dd55fdebee2387918ed
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-03-04 16:50:43 -0500

    Se agrego la seccion de Equipo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,6 +13,7 @@ import picSMotoviajeros from "../../image/Home/pic_motoviajeros2.jpg"
 import picQmision from "../../image/Home/pic_Qmision.png"
 import picQvision from "../../image/Home/pic_Qvision.png"
 import picQvalores from "../../image/Home/pic_Qvalores.png"
+import picTyecid from "../../image/Home/pic_Tyecid.png"
 
 const Home = () => {
     return (
@@ -119,15 +120,52 @@ const Home = () => {
                 {/* ------------------- */}
             </div>
         </section>
+        
 
         <Footer></Footer>
-
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+                <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Nuestro equipo</h2>
+                    <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400"> "Descubre quiénes son las personas detrás de este inspirador proyecto." </p>
+                </div> 
+                <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    <div class="text-center text-gray-500 dark:text-gray-400">
+                        <img class="mx-auto mb-4 w-36 h-36 rounded-full" src={ picTyecid } alt="Foto de perfil Yecid"/>
+                        <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            <a href="#"> Yecid Pardo </a>
+                        </h3>
+                        <p> Scrum Master </p>
+                        <ul class="flex justify-center mt-4 space-x-4">
+                            <li>
+                                <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
+                                </a>
+                            </li>
+                            <li>
+                            <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
+                                <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                                    <image href="" x="0" y="0" height="24" width="24"/>
+                                </svg>
+                            </a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                                </a>
+                            </li> 
+                        </ul>
+                    </div>
+                    
+                </div> 
+            </div>
+        </section>
         {/* Boton para que el usuario sea enviado al principio de la pagina */}
         <section>
             <a href="#">
-                <button className={'fixed bottom-8 right-8 bg-blue-500 hover:bg-black text-white hover:text-amarillo px-4 py-2 rounded-full transition duration-300'}>
+                <button className={'fixed bottom-8 right-8 bg-blue-500 hover:bg-black text-white hover:text-amarillo px-2 py-2 rounded-full transition duration-300'}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18"/>
                     </svg>
                 </button>
             </a>
