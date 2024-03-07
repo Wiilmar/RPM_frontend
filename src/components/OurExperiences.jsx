ce8b93dd97f7614a47b862345e4964af6c43e8fc
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-03-07 13:51:50 -0500

    Se modifico el texto de manera justificado para este mas centrado

--- src/components/OurExperiences.jsx
+++ src/components/OurExperiences.jsx
@@ -16,17 +16,17 @@ const OurExperiences = () => {
                     <div className="p-6">
                         <img src={sujetoUno} className="h-28 border border-gray-300 rounded-lg" />
                         <h2 className="mx-auto py-4 text-2xl font-medium leading-none tracking-tighter text-amarilloTitle lg:text-3xl">Jack Ryder</h2>
-                        <p className="mx-auto text-2xl leading-relaxed text-gray-500">RPM hizo que mi viaje en moto fuera increíblemente suave. Con su ayuda, pude trazar rutas emocionantes a través de paisajes impresionantes y descubrir lugares increíbles en cada parada. ¡Definitivamente es la aplicación perfecta para cualquier motoviajero!</p>
+                        <p className="mx-auto text-2xl leading-relaxed text-gray-500 text-justify">RPM hizo que mi viaje en moto fuera increíblemente suave. Con su ayuda, pude trazar rutas emocionantes a través de paisajes impresionantes y descubrir lugares increíbles en cada parada. ¡Definitivamente es la aplicación perfecta para cualquier motoviajero!</p>
                     </div>
                     <div className="p-6">
                         <img  src={sujetoDos}  className="h-28 border border-gray-300 rounded-lg"/>
                         <h2 className="mx-auto py-4 text-2xl font-medium leading-none tracking-tighter text-amarilloTitle lg:text-3xl">Alexander Kornovich</h2>
-                        <p className="mx-auto text-2xl leading-relaxed text-gray-500">Fue mi compañero constante en mi travesía en moto. Me mantuvo en el camino correcto, me alertó sobre condiciones peligrosas y me ayudó a encontrar los mejores lugares para descansar y disfrutar del paisaje. ¡Una aplicación imprescindible para cualquier motoviajero aventurero!</p>
+                        <p className="mx-auto text-2xl leading-relaxed text-gray-500 text-justify">Fue mi compañero constante en mi travesía en moto. Me mantuvo en el camino correcto, me alertó sobre condiciones peligrosas y me ayudó a encontrar los mejores lugares para descansar y disfrutar del paisaje. ¡Una aplicación imprescindible para cualquier motoviajero aventurero!</p>
                     </div>
                     <div className="p-6">
                         <img  src={sujetaTres}  className="h-28 border border-gray-300 rounded-lg"/>
                         <h2 className="mx-auto py-4 text-2xl font-medium leading-none tracking-tighter text-amarilloTitle lg:text-3xl">Alejandra Rodriguez</h2>
-                        <p className="mx-auto text-2xl leading-relaxed text-gray-500">Con su ayuda, pude planificar rutas emocionantes y seguras, encontrar alojamiento y restaurantes de calidad, y disfrutar al máximo de cada destino. ¡Recomendaría esta aplicación a cualquier motoviajero que quiera una experiencia inolvidable!</p>
+                        <p className="mx-auto text-2xl leading-relaxed text-gray-500 text-justify">Con su ayuda, pude planificar rutas emocionantes y seguras, encontrar alojamiento y restaurantes de calidad, y disfrutar al máximo de cada destino. ¡Recomendaría esta aplicación a cualquier motoviajero que quiera una experiencia inolvidable!</p>
                     </div>
                 </div>
             </div>
