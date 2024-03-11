9664369b42bd7717be16ff01ab7cae4a97229869
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-03-10 21:36:57 -0500

    Se hicieron cambios de opacidad del fondo en el componente ContacUs

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -51,7 +51,11 @@ const ContactUs = () => {
 
 
       <section>
-        <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 relative" style={{ backgroundImage: `url('${picPQRS}')`, backgroundPosition: "center", backgroundSize: "cover" }}>
+        <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12 relative bg-center bg-cover"
+          style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${ picPQRS })`
+          }}
+        >
           <div className="relative py-3 sm:max-w-xl sm:mx-auto">
             <div className="absolute inset-0 bg-gradient-to-r from-indigo-700 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
             <div className="text-white relative px-4 py-10 bg-indigo-400 shadow-lg sm:rounded-3xl sm:p-20">
