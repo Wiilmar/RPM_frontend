2c6d6d59f671267176c6e7c684a9a2f6fc575e29
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-03-13 19:40:49 -0500

    Mejora en el dashboard con pequellos detalles, ademas se configuro de manera que se puedan traer otros componentes al dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './style.css';
+import Pqrs from "../Pqrs/Pqrs"
 
 const Dashboard = () => {
-  const [selectedCard, setSelectedCard] = useState('Dashboard');
+  const [selectedCard, setSelectedCard] = useState('panelPrincipal');
 
   return (
     <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
@@ -19,7 +20,7 @@ const Dashboard = () => {
                 <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
             </button>
-            <input type="search" placeholder="Search" className="w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent" />
+            <input type="search" placeholder="Buscar" className="w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent" />
           </div>
           <ul className="flex items-center">
             <li>
@@ -32,7 +33,7 @@ const Dashboard = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
                   </svg>
                 </span>
-                Logout
+                Cerrar sesi&#243;n
               </a>
             </li>
           </ul>
@@ -50,56 +51,56 @@ const Dashboard = () => {
             </li>
             <li>
               <button
-                onClick={() => setSelectedCard('Dashboard')}
+                onClick={() => setSelectedCard('panelPrincipal')}
                 className={`relative flex flex-row items-center h-11 w-full focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6 ${selectedCard === 'Dashboard' ? 'bg-blue-800 dark:bg-gray-600' : ''}`}
               >
                 <span className="inline-flex justify-center items-center ml-4">
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                 </span>
-                <span className="ml-2 text-sm tracking-wide truncate">Dashboard</span>
+                <span className="ml-2 text-sm tracking-wide truncate">Panel principal</span>
               </button>
             </li>
             <li>
               <button
-                onClick={() => setSelectedCard('Board')}
+                onClick={() => setSelectedCard('gasolina')}
                 className={`relative flex flex-row items-center h-11 w-full focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6 ${selectedCard === 'Board' ? 'bg-blue-800 dark:bg-gray-600' : ''}`}
               >
                 <span className="inline-flex justify-center items-center ml-4">
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
                 </span>
-                <span className="ml-2 text-sm tracking-wide truncate">Board</span>
+                <span className="ml-2 text-sm tracking-wide truncate"> Gasolina </span>
               </button>
             </li>
             <li>
               <button
-                onClick={() => setSelectedCard('Messages')}
+                onClick={() => setSelectedCard('rutas')}
                 className={`relative flex flex-row items-center h-11 w-full focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6 ${selectedCard === 'Messages' ? 'bg-blue-800 dark:bg-gray-600' : ''}`}
               >
                 <span className="inline-flex justify-center items-center ml-4">
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
                 </span>
-                <span className="ml-2 text-sm tracking-wide truncate">Messages</span>
+                <span className="ml-2 text-sm tracking-wide truncate">Rutas</span>
               </button>
             </li>
             <li>
               <button
-                onClick={() => setSelectedCard('Notifications')}
+                onClick={() => setSelectedCard('pqrs')}
                 className={`relative flex flex-row items-center h-11 w-full focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6 ${selectedCard === 'Notifications' ? 'bg-blue-800 dark:bg-gray-600' : ''}`}
               >
                 <span className="inline-flex justify-center items-center ml-4">
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
                 </span>
-                <span className="ml-2 text-sm tracking-wide truncate">Notifications</span>
+                <span className="ml-2 text-sm tracking-wide truncate">P.Q.R.S.</span>
               </button>
             </li>
           </ul>
-          <p className="mb-14 px-5 py-3 hidden md:block text-center text-xs">Copyright @2021</p>
+          <p className="mb-14 px-5 py-3 hidden md:block text-center text-xs">Rutas Para Moteros @2024</p>
         </div>
       </div>
       {/* -------- */}
       <div className="h-full ml-14 mt-14 mb-10 md:ml-64">
-        {/* Card 1 */}
-        {selectedCard === 'Dashboard' && (
+        {/* Panel Principal */}
+        {selectedCard === 'panelPrincipal' && (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
             <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
               <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
@@ -113,8 +114,8 @@ const Dashboard = () => {
           </div>
         )}
         {/* -------- */}
-        {/* Card 2 */}
-        {selectedCard === 'Board' && (
+        {/* Gasolina */}
+        {selectedCard === 'gasolina' && (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
             <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
               <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
@@ -128,8 +129,8 @@ const Dashboard = () => {
           </div>
         )}
         {/* -------- */}
-        {/* Card 3 */}
-        {selectedCard === 'Messages' && (
+        {/* Rutas */}
+        {selectedCard === 'rutas' && (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
             <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
               <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
@@ -143,19 +144,9 @@ const Dashboard = () => {
           </div>
         )}
         {/* -------- */}
-        {/* Card 4 */}
-        {selectedCard === 'Notifications' && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
-            <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-              <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-                <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
-              </div>
-              <div className="text-right">
-                <p className="text-2xl">444</p>
-                <p>Card 4</p>
-              </div>
-            </div>
-          </div>
+        {/* PQRS */}
+        {selectedCard === 'pqrs' && (
+          <Pqrs />
         )}
         {/* -------- */}
       </div>
