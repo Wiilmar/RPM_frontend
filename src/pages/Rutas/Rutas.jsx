c56ed25c41f0e4ee117ca935693d57b2f54464fc
Daniel <danielfelipeluborealpe@gmail.com>
2024-03-19 02:48:07 -0500

    uy mano

--- src/pages/Rutas/Rutas.jsx
+++ src/pages/Rutas/Rutas.jsx
@@ -16,12 +16,13 @@ const Rutas = () => {
 
   return (    
     <>
-    <div className="px-5 py-4">
-      <h1 className="text-3xl py-8 text-white font-bold mb-4">Ultimas Rutas hechas por los usuarios</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {rutas.map((ruta) => (
-            <div key={ruta._id} className="bg-white rounded-lg shadow-md overflow-hidden">
-            <img src={ruta.FotoRuta} className="w-full h-48 object-cover"/>
+  <div className="px-5 py-4">
+    <h1 className="text-3xl py-8 text-white font-bold mb-4">Ultimas Rutas hechas por los usuarios</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {rutas.map((ruta) => (
+        <div key={ruta._id} className="relative">
+          <div className="bg-white rounded-lg shadow-md overflow-hidden transition hover:scale-105">
+            <img src={ruta.FotoRuta} className="w-full h-48 object-cover" alt="Ruta"/>
             <div className="p-4">
               <h2 className="text-xl font-bold mb-2">Nombre de la Ruta: {ruta.NombreRuta}</h2>
               <p className="text-gray-600 mb-1">Calificación: {ruta.CalificacionRuta}</p>
@@ -32,10 +33,12 @@ const Rutas = () => {
               <p className="text-gray-600 mb-1">Moto viajero: {ruta.motoviajero}</p>
             </div>
           </div>
-          ))}
         </div>
+      ))}
     </div>
-    </>
+  </div>
+</>
+
   );
 };
 
