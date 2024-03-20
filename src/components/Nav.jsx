85bf9f9d0ac1a5028aa6ee2a951a5425f830a893
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-03-19 20:27:30 -0500

    Merge realizado con exito

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,9 +31,9 @@ const Nav = () => {
               <Link to="/login" className="font-semibold inline-block bg-blueDark hover:bg-blueLight hover:text-white text-white px-4 py-2 rounded-md transition duration-300 ease-in-out">Administrador</Link>
             </button>
             <div className="md:hidden">
-              <button onClick={toggleMenu} className="mobile-menu-button inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-blueLight focus:outline-none focus:bg-blueDark focus:text-white">
+              <button onClick={ toggleMenu } className="mobile-menu-button inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-blueLight focus:outline-none focus:bg-blueDark focus:text-white">
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"/>
                 </svg>
               </button>
             </div>
