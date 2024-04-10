b105c313efac472877f531205fdca8b51e03a550
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-04-10 16:22:07 -0500

    Implementacion de eliminar ruta

--- src/pages/Rutas/Rutas.jsx
+++ src/pages/Rutas/Rutas.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getRutas } from "../../api/rutas";
+import { getRutas, deleteRutas } from "../../api/rutas";
 
 const Rutas = () => {
   const [rutas, setRutas] = useState([]);
@@ -14,31 +14,48 @@ const Rutas = () => {
     setRutas(response.data.ruta);
   };
 
-  return (    
-    <>
-  <div className="px-5 py-4">
-    <h1 className="text-3xl py-8 text-white font-bold mb-4">Ultimas Rutas hechas por los usuarios</h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {rutas.map((ruta) => (
-        <div key={ruta._id} className="relative">
-          <div className="bg-white rounded-lg shadow-md overflow-hidden transition hover:scale-105">
-            <img src={ruta.FotoRuta} className="w-full h-48 object-cover" alt="Ruta"/>
-            <div className="p-4">
-              <h2 className="text-xl font-bold mb-2">Nombre de la Ruta: {ruta.NombreRuta}</h2>
-              <p className="text-gray-600 mb-1">Calificación: {ruta.CalificacionRuta}</p>
-              <p className="text-gray-600 mb-1">Descripción: {ruta.DescripcionRuta}</p>
-              <p className="text-gray-600 mb-1">Km totales: {ruta.KmTotalesRuta}</p>
-              <p className="text-gray-600 mb-1">Presupuesto: {ruta.PresupuestoGas}</p>
-              <p className="text-gray-600 mb-1">Fecha: {new Date(ruta.createdAt).toLocaleDateString()}</p>
-              <p className="text-gray-600 mb-1">Moto viajero: {ruta.motoviajero}</p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  </div>
-</>
+  const handleDeleteRuta = async (id) => {
+    try {
+      await deleteRutas(id);
+      // Actualiza la lista de rutas después de la eliminación
+      await fetchRutas();
+      alert("Ruta eliminada exitosamente");
+    } catch (error) {
+      console.error("Error al eliminar la ruta:", error);
+      alert("Error al eliminar la ruta");
+    }
+  };
 
+  return (
+    <>
+      <div className="px-5 py-4">
+        <h1 className="text-3xl py-8 text-white font-bold mb-4">Ultimas Rutas hechas por los usuarios</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {rutas.map((ruta) => (
+            <div key={ruta._id} className="relative">
+              <div className="bg-white rounded-lg shadow-md overflow-hidden transition hover:scale-105">
+                <img src={ruta.FotoRuta} className="w-full h-48 object-cover" alt="Ruta" />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Nombre de la Ruta: {ruta.NombreRuta}</h2>
+                  <p className="text-gray-600 mb-1">Calificación: {ruta.CalificacionRuta}</p>
+                  <p className="text-gray-600 mb-1">Descripción: {ruta.DescripcionRuta}</p>
+                  <p className="text-gray-600 mb-1">Km totales: {ruta.KmTotalesRuta}</p>
+                  <p className="text-gray-600 mb-1">Presupuesto: {ruta.PresupuestoGas}</p>
+                  <p className="text-gray-600 mb-1">Fecha: {new Date(ruta.createdAt).toLocaleDateString()}</p>
+                  <p className="text-gray-600 mb-1">Moto viajero: {ruta.motoviajero}</p>
+                  <button
+                    onClick={() => handleDeleteRuta(ruta._id)}
+                    className="absolute top-0 right-0 bg-red-500 text-white p-2 rounded-md hover:bg-red-600"
+                  >
+                    Eliminar
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
