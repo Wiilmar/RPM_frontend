c4500b55e19ac33f34b5514b92fa4c217cb8ecc5
wilman <130717547+Wiilmar@users.noreply.github.com>
2024-04-14 17:47:02 -0500

    Cambios en responsividad de botones en el componente Gallery

--- src/pages/Rutas/Rutas.jsx
+++ src/pages/Rutas/Rutas.jsx
@@ -50,10 +50,10 @@ const Rutas = () => {
     <>
       <div className="px-5 py-4">
         <h1 className="text-3xl py-8 text-white font-bold mb-4">Ultimas Rutas hechas por los usuarios</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 grid-auto-rows-auto">
           {rutas.map((ruta) => (
-            <div key={ruta._id} className="relative">
-              <div className="bg-white rounded-lg shadow-md overflow-hidden transition hover:scale-105">
+            <div key={ruta._id} className="relative h-full">
+              <div className="bg-white rounded-lg shadow-md overflow-hidden transition hover:scale-105 h-full">
                 <img src={ruta.FotoRuta} className="w-full h-48 object-cover" alt="Ruta" />
                 <div className="p-4">
                   <h2 className="text-xl font-bold mb-2">Nombre de la Ruta: {ruta.NombreRuta}</h2>
